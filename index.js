77eba12b856fe0021cb0ff72724b4e5a3c2c504f
Tamm Sjödin <tamm@Mellon.local>
2016-09-04 22:15:52 +0200

    adding ssl

--- index.js
+++ index.js
@@ -4,14 +4,23 @@ var express = require('express');
 var app = express();
 var router = express.Router();
 var routes = require('./routes');
+var https = require('https');
 
 app.use('/static', express.static(__base + 'public'));
 app.set('view engine', 'pug');
 
 app.use('/', routes);
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
-});
+// app.listen(8080, function () {
+//   console.log('Example app listening on port 8080!');
+// });
+
+var privateKey = fs.readFileSync( 'ssl/privatekey.pem' );
+var certificate = fs.readFileSync( 'ssl/certificate.pem' );
+
+https.createServer({
+    key: privateKey,
+    cert: certificate
+}, app).listen(443);
 
 module.exports = router;
